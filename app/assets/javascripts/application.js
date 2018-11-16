6269ccd5506161ff26a2d8c1ce37cde540ab4f6c
toywonder <toy.wonder70@gmail.com>
2018-11-16 20:53:17 +0900

    scrollアニメ製作中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,55 +17,111 @@
 //= require_tree .
 
 $(document).ready(function(){
-  var window_height = $(window).height();
-  var front_skill = $('#front_skill').offset().top-window_height+350;
-  var back_skill = $('#back_skill').offset().top-window_height+250;
-  var server_skill = $('#server_skill').offset().top-window_height+300;
-  var project_title = $("#project_title").offset().top;
-  var skill_title = $("#skill_title").offset().top;
-  var act_title = $("#act_title").offset().top;
+  var window_height = $(window).height(),
+      now_scroll_pos = $(this).scrollTop(),
+      front_skill = $('#front_skill').offset().top-window_height+350+605,
+      back_skill = $('#back_skill').offset().top-window_height+250+605,
+      server_skill = $('#server_skill').offset().top-window_height+300+605,
+
+      project_title = $("#project_title").offset().top,
+      skill_title = $("#skill_title").offset().top+605,
+      act_title = $("#act_title").offset().top+605,
+
+      tedradio = window_height - project_title,
+      metagallery = tedradio+200,
+      cafepedia = metagallery+200;
+
+    front_skill_desplay(now_scroll_pos,front_skill);
+    back_skill_desplay(now_scroll_pos,back_skill);
+    server_skill_desplay(now_scroll_pos,server_skill);
+
+    project_title_desplay(now_scroll_pos,project_title,skill_title);
+    skill_title_desplay(now_scroll_pos,skill_title,act_title);
+    act_title_desplay(now_scroll_pos,act_title);
+
+    tedradio_desplay(now_scroll_pos,tedradio);
+    metagallery_desplay(now_scroll_pos,metagallery);
+    cafepedia_desplay(now_scroll_pos,cafepedia);
 
   $(window).scroll(function() {
     var now_scroll_pos = $(this).scrollTop();
+    front_skill_desplay(now_scroll_pos,front_skill);
+    back_skill_desplay(now_scroll_pos,back_skill);
+    server_skill_desplay(now_scroll_pos,server_skill);
 
-    if(now_scroll_pos >= front_skill){
-      $("#html_progress").addClass("lv_five");
-      $("#css_progress").addClass("lv_five");
-      $("#js_progress").addClass("lv_four");
-      $("#jquery_progress").addClass("lv_four");
-      $("#vue_progress").addClass("lv_three");
-    }
+    project_title_desplay(now_scroll_pos,project_title,skill_title);
+    skill_title_desplay(now_scroll_pos,skill_title,act_title);
+    act_title_desplay(now_scroll_pos,act_title);
 
-    if(now_scroll_pos >= back_skill){
-      $("#ruby_progress").addClass("lv_four");
-      $("#ror_progress").addClass("lv_four");
-    }
-
-    if(now_scroll_pos >= server_skill){
-      $("#heroku_progress").addClass("lv_three");
-      $("#aws_progress").addClass("lv_two");
-    }
-
-    if(now_scroll_pos >= project_title && now_scroll_pos <= skill_title){
-      $("#project_title").addClass("section_title_top");
-      $("#skill_title").removeClass("section_title_top");
-      $("#act_title").removeClass("section_title_top");
-    }else{
-      $("#project_title").removeClass("section_title_top");
-    }
-    if(now_scroll_pos >= skill_title && now_scroll_pos <= act_title){
-      $("#skill_title").addClass("section_title_top");
-      $("#project_title").removeClass("section_title_top");
-      $("#act_title").removeClass("section_title_top");
-    }
-    if(now_scroll_pos >= act_title){
-      $("#act_title").addClass("section_title_top");
-      $("#skill_title").removeClass("section_title_top");
-      $("#project_title").removeClass("section_title_top");
-    }
+    tedradio_desplay(now_scroll_pos,tedradio);
+    metagallery_desplay(now_scroll_pos,metagallery);
+    cafepedia_desplay(now_scroll_pos,cafepedia);
   });
-
 });
+
+function front_skill_desplay(now_scroll_pos,front_skill){
+  if(now_scroll_pos >= front_skill){
+    $("#html_progress").addClass("lv_five");
+    $("#css_progress").addClass("lv_five");
+    $("#js_progress").addClass("lv_four");
+    $("#jquery_progress").addClass("lv_four");
+    $("#vue_progress").addClass("lv_three");
+  }
+}
+function back_skill_desplay(now_scroll_pos,back_skill){
+  if(now_scroll_pos >= back_skill){
+    $("#ruby_progress").addClass("lv_four");
+    $("#ror_progress").addClass("lv_four");
+  }
+}
+
+function server_skill_desplay(now_scroll_pos,server_skill){
+  if(now_scroll_pos >= server_skill){
+    $("#heroku_progress").addClass("lv_three");
+    $("#aws_progress").addClass("lv_two");
+  }
+}
+
+function project_title_desplay(now_scroll_pos,project_title,skill_title){
+  if(now_scroll_pos >= project_title && now_scroll_pos <= skill_title){
+    $("#project_title").addClass("section_title_top");
+    $("#skill_title").removeClass("section_title_top");
+    $("#act_title").removeClass("section_title_top");
+  }else{
+    $("#project_title").removeClass("section_title_top");
+  }
+}
+function skill_title_desplay(now_scroll_pos,skill_title,act_title){
+  if(now_scroll_pos >= skill_title && now_scroll_pos <= act_title){
+    $("#skill_title").addClass("section_title_top");
+    $("#project_title").removeClass("section_title_top");
+    $("#act_title").removeClass("section_title_top");
+  }
+}
+
+function act_title_desplay(now_scroll_pos,act_title){
+  if(now_scroll_pos >= act_title){
+    $("#act_title").addClass("section_title_top");
+    $("#skill_title").removeClass("section_title_top");
+    $("#project_title").removeClass("section_title_top");
+  }
+}
+
+function tedradio_desplay(now_scroll_pos,tedradio){
+  if(now_scroll_pos >= tedradio){
+    $("#tedradio_project").fadeIn("slow");
+  }
+}
+function metagallery_desplay(now_scroll_pos,metagallery){
+  if(now_scroll_pos >= metagallery){
+    $("#metagallery_project").fadeIn("slow");
+  }
+}
+function cafepedia_desplay(now_scroll_pos,cafepedia){
+  if(now_scroll_pos >= cafepedia){
+    $("#cafepedia_project").fadeIn("slow");
+  }
+}
 
 function booklog_minishelf(json) {
   var items = json["books"];
